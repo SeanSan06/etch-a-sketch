22729eadd6cc8dd757935b28fbeab102443d0b69
Sean San <seanksan@gmail.com>
2025-10-10 22:42:11 -0700

    Add the ability to color each cell in black

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const drawingArea = document.querySelector("#drawing-area");
 function grid() {
     for(let i = 0; i < 64; ++i) {
         const gridBox = document.createElement("div");
-        gridBox.style.border = "1px solid blue";
+        // gridBox.style.border = "1px solid blue";
         drawingArea.appendChild(gridBox);
+
+        gridBox.addEventListener("mouseover", () => {
+            gridBox.style.backgroundColor = "black";
+        });
     }
 }
 
