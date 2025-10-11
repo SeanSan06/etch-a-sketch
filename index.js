236506bc60cc0e13c9f47cebedf9b89f57fd7ae6
Sean San <seanksan@gmail.com>
2025-10-10 23:26:06 -0700

    Switch from using grids to flexbox, for practice
    Display: grid; is a better practice, but this change was done to get
    more comfortable with flexbox.

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const drawingArea = document.querySelector("#drawing-area");
 function grid() {
-    for(let i = 0; i < 64; ++i) {
+    for(let i = 0; i < 256; ++i) {
         const gridBox = document.createElement("div");
         // gridBox.style.border = "1px solid blue";
         drawingArea.appendChild(gridBox);
